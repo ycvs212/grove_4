cfe41778a54dd5cc4facfa4d2febcedc6fd5e556
ycvs212 <59155778+ycvs212@users.noreply.github.com>
2020-10-17 16:52:43 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+})
+input.onButtonPressed(Button.B, function () {
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+})
+basic.forever(function () {
+	
+})
